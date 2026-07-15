669680bbaa557442ba0bada1f15d20901443661d
Lam Nguyen <khoalamvn@gmail.com>
2026-07-15 17:15:48 +0700

    Update ignore and add PORT env var

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 cd "$(dirname "$0")"
 export MODEL_DIR="${MODEL_DIR:-models}"
-./venv/Scripts/python.exe -m uvicorn main:app --host 0.0.0.0 --port 8000
+PORT="${PORT:-8000}"
+./venv/Scripts/python.exe -m uvicorn main:app --host 0.0.0.0 --port "$PORT"
